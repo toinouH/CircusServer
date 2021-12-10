a32d49b112ef1ca31e262920b9262865f4ebb233
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2021-12-10 15:49:47 -0700

    Oops, Reformmat match api class as well.

--- src/main/java/circus/api/Match.java
+++ src/main/java/circus/api/Match.java
@@ -9,42 +9,25 @@ public class Match {
     private ArrayList<String> red_team;
     private String map;
     private String state;
+    private String outcome;
     private int blue_captain;
     private int red_captain;
 
-    public String getTimestamp() {
-        return timestamp;
-    }
+    public String getTimestamp()                            { return timestamp;             }
 
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
+    public void setTimestamp(String timestamp)              { this.timestamp = timestamp;   }
 
-    public ArrayList<String> getBlueTeam() {
-        return blue_team;
-    }
+    public ArrayList<String> getBlueTeam()                  { return blue_team;             }
 
-    public void setBlueTeam(ArrayList<String> blue_team) {
-        this.blue_team = blue_team;
-    }
+    public void setBlueTeam(ArrayList<String> blue_team)    { this.blue_team = blue_team;   }
 
-    public ArrayList<String> getRedTeam() {
-        return red_team;
-    }
+    public ArrayList<String> getRedTeam()                   { return red_team;              }
 
-    public void setRedTeam(ArrayList<String> red_team) {
-        this.red_team = red_team;
-    }
+    public void setRedTeam(ArrayList<String> red_team)      { this.red_team = red_team;     }
 
-    private String outcome;
+    public int getId()                                      { return id;                    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id)                               { this.id = id;                 }
 
     // This is ass this is not going live this is going to be nuked just needed to check concept
     public int getMap()
@@ -75,41 +58,21 @@ public class Match {
         };
     }
 
-    public void setMap(String map) {
-        this.map = map;
-    }
+    public void setMap(String map)              { this.map = map;                   }
 
-    public String getOutcome() {
-        return outcome;
-    }
+    public String getOutcome()                  { return outcome;                   }
 
-    public void setOutcome(String outcome) {
-        this.outcome = outcome;
-    }
+    public void setOutcome(String outcome)      { this.outcome = outcome;           }
 
-    public String getState() {
-        return state;
-    }
+    public String getState()                    { return state;                     }
 
-    public void setState(String state) {
-        this.state = state;
-    }
+    public void setState(String state)          { this.state = state;               }
 
-    public int getBlueCaptain() {
-        return blue_captain;
-    }
+    public int getBlueCaptain()                 { return blue_captain;              }
 
-    public void setBlueCaptain(int blueCaptain) {
-        this.blue_captain= blueCaptain;
-    }
+    public void setBlueCaptain(int blueCaptain) { this.blue_captain= blueCaptain;   }
 
-    public int getRedCaptain() {
-        return red_captain;
-    }
+    public int getRedCaptain()                  { return red_captain;               }
 
-    public void setRedCaptain(int redCaptain) {
-        this.red_captain= redCaptain;
-    }
-
-
+    public void setRedCaptain(int redCaptain)   { this.red_captain= redCaptain;     }
 }
